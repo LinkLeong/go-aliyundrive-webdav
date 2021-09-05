8dcb1f398db8a1219abf0f4b8b1b1ea9a8359530
a624669980 <a624669980@163.com>
2021-09-05 22:32:35 +0800

    修改

--- aliyun/model/refresh.go
+++
@@ -1,8 +0,0 @@
-package model
-
-type RefreshTokenModel struct {
-	AccessToken    string `json:"access_token"`
-	DefaultDriveId string `json:"default_drive_id"`
-	RefreshToken   string `json:"refresh_token"`
-	ExpiresIn      int64    `json:"expires_in"`
-}
--- go.mod
+++
@@ -1,5 +0,0 @@
-module go-aliyun
-
-go 1.15
-
-require github.com/patrickmn/go-cache v2.1.0+incompatible
