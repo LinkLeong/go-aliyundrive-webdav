7f77b246455cb0a02c3a32c1a74db2d1d0992c8c
a624669980 <a624669980@163.com>
2021-09-29 12:17:33 +0800

    gcc1

--- main.go
+++ main.go
@@ -8,6 +8,8 @@ import (
 	"go-aliyun-webdav/aliyun/cache"
 	"go-aliyun-webdav/aliyun/model"
 	"go-aliyun-webdav/webdav"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
 	"net/http"
 	"os"
 	"runtime"
@@ -21,7 +23,33 @@ func init() {
 
 var Version = "v1.0.3"
 
+type Task struct {
+	Id string `json:"id"`
+}
+
+func GetDb() *gorm.DB {
+	// 参考 https://github.com/go-sql-driver/mysql#dsn-data-source-name 获取详情
+	//dsn := fmt.Sprintf("%v:%v@tcp(%v:%v)/%v?charset=utf8mb4&parseTime=True&loc=Local", m.User, m.PWD, m.IP, m.Port, m.DBName)
+	//db, err := gorm.Open(mysql2.Open(dsn), &gorm.Config{})
+	db, err := gorm.Open(sqlite.Open("./db/casaOS.db"), &gorm.Config{})
+	c, _ := db.DB()
+	c.SetMaxIdleConns(10)
+	c.SetMaxOpenConns(100)
+	c.SetConnMaxIdleTime(time.Second * 1000)
+	if err != nil {
+		fmt.Println("连接数据失败!")
+		panic("数据库连接失败")
+		return nil
+	}
+	err = db.AutoMigrate(&Task{})
+	if err != nil {
+		fmt.Println("检查和创建数据库出错", err)
+	}
+	return db
+}
+
 func main() {
+	GetDb()
 	var port *string
 	var path *string
 	var refreshToken *string
